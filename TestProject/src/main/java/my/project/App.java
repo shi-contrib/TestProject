3dfa786159afe54b830a1ddf388e2f1b4da8edb0
shi@o2.pl <shi@o2.pl>
2013-07-23 01:06:28 +0200

    done

--- TestProject/src/main/java/my/project/App.java
+++ TestProject/src/main/java/my/project/App.java
@@ -8,8 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        System.out.println( "Hello Shi2nexti222!" );
-
+        System.out.println( "Hello Tests changed" );
     }
 }
