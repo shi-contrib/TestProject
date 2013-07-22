378708e43cdfe37279780737ba10cf126493fe83
shigithub <sv501@o2.pl>
2013-07-23 00:02:16 +0100

    My coommitttt

--- TestProject/src/main/java/my/project/App.java
+++ TestProject/src/main/java/my/project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Tests changed" );
+        System.out.println( "Hello Tests changed 777" );
     }
 }
