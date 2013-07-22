40ca3bb9681226d775ff862f0fe17ddcb75ef4bd
shi@o2.pl <shi@o2.pl>
2013-07-22 23:49:24 +0200

    Revert "testing revertion"
    This reverts commit 94e1cc54c0abfd013f3d91919fbc5b9ad77a0f5e.

--- TestProject/src/main/java/my/project/App.java
+++ TestProject/src/main/java/my/project/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Hello Shi2nextiTestRevert!" );
+        System.out.println( "Hello Shi2nexti222!" );
 
     }
 }
